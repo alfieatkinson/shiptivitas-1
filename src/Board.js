4e7ade032165d43c09b4d49914ffa5f4560a26f8
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-30 11:50:46 +0000

    Refactor client state initialization and move getClients method to improve clarity and maintainability

--- src/Board.js
+++ src/Board.js
@@ -7,34 +7,18 @@ import './Board.css';
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    const clients = this.getClients();
     this.state = {
       clients: {
-        backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
-        complete: clients.filter(client => client.status && client.status === 'complete'),
+        backlog: [],
+        inProgress: [],
+        complete: [],
       }
-    }
+    };
     this.swimlanes = {
       backlog: React.createRef(),
       inProgress: React.createRef(),
       complete: React.createRef(),
-    }
-  }
-
-  getClients() {
-    fetch('http://localhost:3001/api/v1/clients') // Fetching from the backend
-      .then(res => res.json())
-      .then(clients => {
-        this.setState({
-          clients: {
-            backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-            inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
-            complete: clients.filter(client => client.status && client.status === 'complete'),
-          }
-        });
-      })
-      .catch(err => console.error('Failed to fetch clients: ', err));
+    };
   }
 
   renderSwimlane(name, clients, ref) {
@@ -76,6 +60,21 @@ export default class Board extends React.Component {
 
   componentWillUnmount() {
     this.drake.remove();
+  }
+
+  getClients() {
+    fetch('http://localhost:3001/api/v1/clients') // Fetching from the backend
+      .then(res => res.json())
+      .then(clients => {
+        this.setState({
+          clients: {
+            backlog: clients.filter(client => !client.status || client.status === 'backlog'),
+            inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
+            complete: clients.filter(client => client.status && client.status === 'complete'),
+          }
+        });
+      })
+      .catch(err => console.error('Failed to fetch clients: ', err));
   }
 
   // Change the status of client when a Card is moved
