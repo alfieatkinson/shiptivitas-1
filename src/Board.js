738c0be996ee84448bfaf4b37bad7db10e92aeb9
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-30 11:27:40 +0000

    Fetch clients from backend API instead of hardcoded values in getClients method

--- src/Board.js
+++ src/Board.js
@@ -23,33 +23,18 @@ export default class Board extends React.Component {
   }
 
   getClients() {
-    return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'backlog'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'backlog'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'backlog'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'backlog'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'backlog'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'backlog'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'backlog'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'backlog'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'backlog'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
-    ].map(companyDetails => ({
-      id: companyDetails[0],
-      name: companyDetails[1],
-      description: companyDetails[2],
-      status: companyDetails[3],
-    }));
+    fetch('/api/v1/clients') // Fetching from the backend
+      .then(res => res.json())
+      .then(clients => {
+        this.setState({
+          clients: {
+            backlog: clients.filter(client => !client.status || client.status === 'backlog'),
+            inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
+            complete: clients.filter(client => client.status && client.status === 'complete'),
+          }
+        });
+      })
+      .catch(err => console.error(err));
   }
 
   renderSwimlane(name, clients, ref) {
